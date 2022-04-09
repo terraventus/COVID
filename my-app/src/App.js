eddecbeb87cca9dc96ea0a4775bad5fa64495381
terraventus <jinxdangerouss@gmail.com>
2022-04-09 16:28:12 -0300

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StylesProvider } from '@material-ui/styles'
+import { CssBaseline } from '@material-ui/core'
+import GlobalStyle from './commons/styles/global-style'
+
+function App() {
+  return (
+    <StylesProvider injectFirst>
+      <CssBaseline />
+      <GlobalStyle />
+      <div>
+        Hello World
+      </div>
+    </StylesProvider>
+  );
+}
+
+export default App;
+
